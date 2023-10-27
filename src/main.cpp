be8c3864d63a3a90dc8ea44b396df89a995c8162
Spiderbat2089 <ajamescortez@gmail.com>
2023-10-27 17:30:04 -0400

    Version 1.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -34,58 +34,54 @@ float strafe;
 
 void mecanumRun()
 {
-  leftFrontMotor.state(speed + turn + strafe,percent);
-  leftBackMotor.state(speed - turn - strafe,percent);
-  rightFrontMotor.state(speed + turn - strafe,percent);
-  rightBackMotor.state(speed - turn + strafe,percent);
+  leftFrontMotor.state(speed - strafe + turn,percent);
+  leftBackMotor.state(speed + strafe + turn,percent);
+  rightFrontMotor.state(-speed - strafe + turn,percent);
+  rightBackMotor.state(-speed + strafe + turn,percent);
 }
 
 void intakeRun()
 {
-  if(Controller2.ButtonA.pressing())
+  if(Controller2.ButtonB.pressing())
   {
-    //time.clear();
-    if (/*time.time(seconds) <= 3 &&*/ lSwitch.value() == 0)
-    {
-      intakeMotor.state(50, percent);
-    }
-  }
-  else if(Controller2.ButtonY.pressing() || lSwitch.value() == 1)
-  {
-    intakeMotor.state(50, percent);
+    intakeMotor.state(100, percent);
   }
   else
   {
     intakeMotor.state(0, percent);
   }
-    
 }
 void outtakeRun()
 {
-  if(Controller2.ButtonB.pressing())
+  
+  if(Controller2.ButtonA.pressing() && lSwitch.value() == 1)
   {
-    outtakeMotor.state(50, percent);
+    outtakeMotor.state(100, percent);
+  }
+  else if(Controller2.ButtonX.pressing() && lSwitch.value() == 0)
+  {
+    outtakeMotor.state(100, percent);
   }
   else
   {
     outtakeMotor.state(0, percent);
   }
-    
 }
 
 int main() {
 
   // vexcodeInit();
-  Brain.Screen.clearScreen();
+  Controller2.Screen.clearScreen();
 
   // Main Controller loop to set motors to controller axis postiions
   while(true)
   {
-    speed = -Controller1.Axis3.position();
-    turn = Controller1.Axis4.position();
-    strafe = Controller1.Axis1.position();
+    
+    speed = Controller1.Axis3.position();
+    turn = Controller1.Axis1.position();
+    strafe = -Controller1.Axis4.position();
     mecanumRun();
-    // intakeRun();
+    intakeRun();
     outtakeRun();
 
   }
